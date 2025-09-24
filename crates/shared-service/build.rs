88cc637dbaf343180cde7658018022edbf248bff
ccamel55 <allanma31@gmail.com>
2025-09-24 20:08:54 +1200

    chore: switching rpc (again) to tonic

---
+++ crates/shared-service/build.rs
@@ -0,0 +1,27 @@
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    const WORKSPACE_DIR: &str = env!("WORKSPACE_DIR");
+
+    let include_path = PathBuf::from(WORKSPACE_DIR).join("protobufs");
+    let descriptor_path =
+        PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("shared-service-descriptors.bin");
+
+    // We have to manually include the protobuf files we want to use.
+    // todo: search through directory and add all protobuf files automatically
+    let files = vec![
+        include_path.join("echo.proto"),
+        include_path.join("user.proto"),
+    ];
+
+    // Create rust code
+    tonic_prost_build::configure()
+        .build_client(true)
+        .build_server(true)
+        .build_transport(false)
+        .emit_rerun_if_changed(true)
+        .file_descriptor_set_path(descriptor_path)
+        .compile_protos(&files, &[include_path])?;
+
+    Ok(())
+}
--- crates/shared-service/src/lib.rs
+++ crates/shared-service/src/lib.rs
@@ -1,3 +1,10 @@
 mod echo;
 
 pub use echo::*;
+
+/// File descriptor set used for reflection service.
+pub const FILE_DESCRIPTOR_SET: &[u8] =
+    tonic::include_file_descriptor_set!("shared-service-descriptors");
+
+tonic::include_proto!("bw.echo");
+tonic::include_proto!("bw.user");
