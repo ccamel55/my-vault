e7b9433a8ef47b36c6f8b60619864a3f6a0737a0
ccamel55 <allanma31@gmail.com>
2025-10-14 20:28:54 +1300

    chore: improve user table

--- crates/daemon/migrations/20251002103552_create-database.sql
+++ crates/daemon/migrations/20251002103552_create-database.sql
@@ -1,4 +1,29 @@
-CREATE TABLE IF NOT EXISTS users (
-    uuid    BLOB PRIMARY KEY NOT NULL,
-    name    VARCHAR(255) NOT NULL UNIQUE
+-- Create table for users
+CREATE TABLE users (
+    uuid                BLOB NOT NULL,
+    email               TEXT NOT NULL UNIQUE,
+    password_hash       TEXT NOT NULL,
+    first_name          VARCHAR(255) NOT NULL,
+    last_name           VARCHAR(255) NOT NULL,
+    last_updated        TIMESTAMP NOT NULL,
+
+    PRIMARY KEY (uuid)
 );
+
+--
+-- Update last_updated for new entries or when old entries are updated
+--
+
+CREATE TRIGGER users_trigger_after_insert AFTER INSERT ON users
+    BEGIN
+        UPDATE users
+            SET last_updated = DATETIME('NOW')
+            WHERE ROWID = NEW.ROWID;
+    END;
+
+CREATE TRIGGER users_trigger_after_update AFTER UPDATE ON users
+    BEGIN
+        UPDATE users
+            SET last_updated = DATETIME('NOW')
+            WHERE ROWID = NEW.ROWID;
+    END;
